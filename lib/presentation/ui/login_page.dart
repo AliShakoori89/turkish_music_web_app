00dae20c548a341f7cd4514f8de7106e3a61e743
AliShakoori89 <53476225+AliShakoori89@users.noreply.github.com>
2024-01-23 09:57:23 +0330

    edit text field error

--- lib/presentation/ui/login_page.dart
+++ lib/presentation/ui/login_page.dart
@@ -1,16 +1,28 @@
 import 'package:email_validator/email_validator.dart';
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
   @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+
+  final emailController = TextEditingController();
+  final emailFormKey = GlobalKey<FormState>();
+
+  @override
+  void initState() {
+    super.initState();
+
+  }
+
+  @override
   Widget build(BuildContext context) {
-
-    final emailController = TextEditingController();
-    final emailFormKey = GlobalKey<FormState>();
-
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       body: Container(
         margin: const EdgeInsets.all(10),
         child: Column(
@@ -18,34 +30,51 @@ class LoginPage extends StatelessWidget {
           children: [
             Form(
                 key: emailFormKey,
-                child: TextFormField(
-                  cursorColor: Colors.deepOrange,
-                  controller: emailController,
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return 'Please enter email';
-                    }
-                    if (EmailValidator.validate(value)){
+                child: SizedBox(
+                  child: TextFormField(
+                    cursorColor: Colors.grey,
+                    controller: emailController,
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Please enter email';
+                      }
+                      else if (EmailValidator.validate(value)){
+                        return null ;
+                      }
                       return "Please enter a valid email";
-                    }
-                    return null ;
-
-                  },
-                  autocorrect: false,
-                  decoration: const InputDecoration(
-                    enabledBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(color: Colors.white)),
-                    hintText: "Email",
-                    hintStyle: (TextStyle(color: Colors.white30)),
-                    icon: Icon(
-                      Icons.email,
-                      color: Colors.white,
-                    ),
-                    focusedBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(color: Colors.deepOrange),
+                    },
+                    autocorrect: false,
+                    decoration: const InputDecoration(
+                      contentPadding:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+                      border: InputBorder.none,
+                      enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white)),
+                      hintText: "Email",
+                      hintStyle: (TextStyle(color: Colors.white30)),
+                      icon: Icon(
+                        Icons.email,
+                        color: Colors.white,
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey),
+                      ),
+                      errorBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(4)),
+                          borderSide: BorderSide(
+                            width: 1,
+                            color: Colors.red,
+                          )),
+                      focusedErrorBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(4)),
+                        borderSide: BorderSide(
+                          width: 1,
+                          color: Colors.red,
+                        )),
                     ),
                   ),
-                )),
+                )
+            ),
             Padding(
               padding: EdgeInsets.only(
                   top: MediaQuery.of(context).size.height * 0.03),
