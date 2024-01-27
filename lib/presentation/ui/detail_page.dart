91dfe1e724f83be5c284946f896a72e13c97fa99
AliShakoori89 <53476225+AliShakoori89@users.noreply.github.com>
2024-01-27 16:29:32 +0330

    show dialog box

--- lib/presentation/ui/detail_page.dart
+++ lib/presentation/ui/detail_page.dart
@@ -51,7 +51,7 @@ class DetailPage extends StatelessWidget {
                       ),
                       Flexible(
                         child: IconButton(
-                            onPressed: (){},
+                            onPressed: () => BottomDialog().showBottomDialog(context),
                             icon: Icon(Icons.more_vert,
                             size: 20,)),
                       )
@@ -59,6 +59,45 @@ class DetailPage extends StatelessWidget {
                   )),
         ),
       ),
+    );
+  }
+}
+
+class BottomDialog {
+  void showBottomDialog(BuildContext context) {
+    showGeneralDialog(
+      barrierLabel: "showGeneralDialog",
+      barrierDismissible: true,
+      barrierColor: Colors.black.withOpacity(0.6),
+      transitionDuration: const Duration(milliseconds: 400),
+      context: context,
+      pageBuilder: (context, _, __) {
+        return Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+              width: double.maxFinite,
+              height: MediaQuery.of(context).size.height * 0.3,
+              clipBehavior: Clip.antiAlias,
+              padding: const EdgeInsets.all(16),
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(16),
+                  topRight: Radius.circular(16),
+                ),
+              ),
+            )
+        );
+      },
+      transitionBuilder: (_, animation1, __, child) {
+        return SlideTransition(
+          position: Tween(
+            begin: const Offset(0, 1),
+            end: const Offset(0, 0),
+          ).animate(animation1),
+          child: child,
+        );
+      },
     );
   }
 }
