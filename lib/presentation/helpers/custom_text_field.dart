837dacfbc651b170d1672f668044cffcbdd9e81c
Ali.sh <alishakoori89@gmail.com>
2024-02-25 00:28:05 +0330

    login page chang

--- lib/presentation/helpers/custom_text_field.dart
+++ lib/presentation/helpers/custom_text_field.dart
@@ -8,40 +8,20 @@ class CustomTextField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(20)
-      ),
-      margin: EdgeInsets.only(
-          right: MediaQuery.of(context).size.width * 0.05,
-          left: MediaQuery.of(context).size.width * 0.05
-      ),
-      child: TextFormField(
-        cursorColor: Colors.grey,
-        controller: emailController,
-        validator: (value) {
-          if (value!.isEmpty) {
-            return 'Enter email';
-          }
-          else if (EmailValidator.validate(value)){
-            return null ;
-          }
-          return "Enter a valid email";
-        },
-        autocorrect: false,
-        decoration: InputDecoration(
-          border: OutlineInputBorder(
-              borderRadius: BorderRadius.all(Radius.circular(20))),
-          hintText: 'type your title',
-          hintStyle: const TextStyle(
-              color: Color.fromRGBO(215, 215, 215, 1)
-          ),
-          errorStyle: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.w500),
-          filled: true,
-        ),
-      ),
+    return TextFormField(
+      decoration: const InputDecoration(hintText: "Email"),
+      cursorColor: Colors.grey,
+      controller: emailController,
+      validator: (value) {
+        if (value!.isEmpty) {
+          return 'Enter email';
+        }
+        else if (EmailValidator.validate(value)){
+          return null ;
+        }
+        return "Enter a valid email";
+      },
+      autocorrect: false,
     );
   }
 }
