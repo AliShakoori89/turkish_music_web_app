2fa29874ae5e620b966eb4ccda9a07f4a0ca6590
AliShakoori89 <53476225+AliShakoori89@users.noreply.github.com>
2024-04-27 12:07:24 +0330

    add category repository

---
+++ lib/domain/repositories/category_repository.dart
@@ -0,0 +1,17 @@
+import 'dart:convert';
+import '../../data/model/category_model.dart';
+import '../../data/network/api_base_helper.dart';
+
+class CategoryRepository {
+
+  final String apiKey = 'YekAdadApiKeyMibashadKeBarayeApplicationTurkishMusicJahatEstefadehAsApiHaSakhteShodeAst';
+
+  @override
+  Future<dynamic> getAllCategory() async {
+    ApiBaseHelper api = ApiBaseHelper();
+    final response = await api.get('/api/Category/GetAll');
+    final productJson = json.decode(response.body);
+    var allCategory = CategoryDataModel.fromJson(productJson);
+    return allCategory;
+  }
+}
