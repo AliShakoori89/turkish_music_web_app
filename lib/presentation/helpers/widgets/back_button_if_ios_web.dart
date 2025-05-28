c01ed8928870ef30ba7b169a1ab9d4fd599051b1
a.shakouri <alishakoori89@gmail.com>
2025-05-28 15:31:06 +0330

    add back button for ios

--- lib/presentation/helpers/widgets/back_button_if_ios_web.dart
+++ lib/presentation/helpers/widgets/back_button_if_ios_web.dart
@@ -5,14 +5,14 @@ import 'package:flutter/material.dart';
 Widget? buildBackButtonIfIosWeb() {
   if (kIsWeb) {
     final userAgent = html.window.navigator.userAgent.toLowerCase();
-    // if (userAgent.contains('iphone') || userAgent.contains('ipad')) {
+    if (userAgent.contains('iphone') || userAgent.contains('ipad')) {
       return IconButton(
         icon: Icon(Icons.arrow_back_ios_new_sharp),
         onPressed: () {
           html.window.history.back();
         },
       );
-    // }
+    }
   }
   return null;
 }
